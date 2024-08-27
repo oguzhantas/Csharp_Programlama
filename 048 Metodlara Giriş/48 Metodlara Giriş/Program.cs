1f2c6dc8f3a233e4ef487f0ca2d4b87a49ec5400
Oguzhan TAS <oguzhantas@gmail.com>
2024-08-27 16:54:57 +0300

    Add files via upload

---
+++ 048 Metodlara Giriş/48 Metodlara Giriş/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48_Metodlara_Giriş
+{
+    internal class Program
+    {
+        static void merhaba()
+        {
+            Console.WriteLine("Merhaba");
+        }
+        static void Main(string[] args)
+        {
+
+            merhaba();
+            merhaba();
+            Console.ReadKey();
+
+        }
+    }
+}
